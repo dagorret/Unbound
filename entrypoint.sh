38b2e5aa585a7ac052d7f6c27dddd5e1138b6bfa
Carlos Dagorret <dagorret@gmail.com>
2025-07-08 01:44:41 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,10 @@ echo "[INFO] Actualizando root.hints desde https://www.internic.net/domain/named
 curl -s https://www.internic.net/domain/named.root -o /etc/unbound/root.hints
 echo "[OK] Archivo root.hints actualizado correctamente."
 
-if [ -f /etc/unbound/unbound-control-setup.sh ]; then
-    echo "[INFO] Configurando control remoto..."
-    sh /etc/unbound/unbound-control-setup.sh
-fi
+#if [ -f /etc/unbound/unbound-control-setup.sh ]; then
+#   echo "[INFO] Configurando control remoto..."
+#   sh /etc/unbound/unbound-control-setup.sh
+#fi
 
 echo "[INFO] Iniciando Unbound..."
 exec unbound -d -c /etc/unbound/unbound.conf
